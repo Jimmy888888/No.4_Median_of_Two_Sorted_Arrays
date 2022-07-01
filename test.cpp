2ce0533aa820ba63f1d0323363cd8b65aa6bf9f0
康任均 <kangrenjun@kangrenjundeMacBook-Air.local>
2022-07-01 22:13:58 +0800

    2022/07/01 update05 No.4 c++

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-    
+
     sort(nums1.begin(), nums1.end());
     int n = nums1.size();        
     double ans;
@@ -21,27 +21,27 @@ public:
     }
 };
 
-int main() 
-{ 
-    // initialising the vector 
-    vector<int> vec1; 
-    vector<int>::iterator i_vec1;
-    vector<int> vec2; 
-    vec1.push_back(10);
-    vec1.push_back(20);
-    vec1.push_back(30);
-    vec1.push_back(40);
+// int main() 
+// { 
+//     // initialising the vector 
+//     vector<int> vec1; 
+//     vector<int>::iterator i_vec1;
+//     vector<int> vec2; 
+//     vec1.push_back(10);
+//     vec1.push_back(20);
+//     vec1.push_back(30);
+//     vec1.push_back(40);
   
-    // inserts at the beginning of vec2 
-    vec2.insert(vec2.begin(), vec1.begin(), vec1.end()); 
+//     // inserts at the beginning of vec2 
+//     vec2.insert(vec2.begin(), vec1.begin(), vec1.end()); 
 
-    vec1[0] = 2;
+//     vec1[0] = 2;
   
-    cout << "The vector2 elements are: "; 
-    for (i_vec1 = vec2.begin(); i_vec1 != vec2.end(); ++i_vec1) 
-        cout << *i_vec1 << " "; 
+//     cout << "The vector2 elements are: "; 
+//     for (i_vec1 = vec2.begin(); i_vec1 != vec2.end(); ++i_vec1) 
+//         cout << *i_vec1 << " "; 
     
-    cout << endl << vec1[0];
+//     cout << endl << vec1[0];
   
-    return 0; 
-}
+//     return 0; 
+// }
